59eeba6c59b4ed95eb2cd11c40b6c168dc52e3b3
Ritesh Sinha <ritesh.kumar.sinha@gmail.com>
2024-06-01 07:33:35 +0100

    Update app.py
    Add bling bling

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from app import app
+import app
+import time
 
-class HelloWorld(App):
-  def __init__():
-    pass
+from events.input import Buttons, BUTTON_TYPES
+from tildagonos import tildagonos
 
-  def update():
-    pass:
 
-  def draw():
-    ctx.move_to(0, 0).text("Hello, world!"]
 
-__app_export__ = HelloWorld
+class Bling(app.App):
+    def __init__(self):
+        self.button_states = Buttons(self)
+
+    def update(self, delta):
+        if self.button_states.get(BUTTON_TYPES["CANCEL"]):
+            self.button_states.clear()
+            self.minimise()
+        for i in range(0,12):
+                time.sleep(0.05)
+                tildagonos.leds[i+1] = (255, 215, 0)
+        
+
+    def draw(self, ctx):
+        ctx.save()
+        ctx.font_size = 90
+        ctx.rgb(250, 250, 210).rectangle(-120,-120,240,240).fill()
+        ctx.rgb(255, 215, 0).move_to(-100,20).text("Bling!")
+        ctx.restore()
+
+__app_export__ = Bling 
